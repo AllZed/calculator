52de8a8f708f600386c69a0a70c9bd8fb15f226e
Sam <34135488+AllZed@users.noreply.github.com>
2020-09-06 05:57:24 -0400

    Update functions.cpp
    changed if else functions to switch.

--- functions.cpp
+++ functions.cpp
@@ -5,27 +5,28 @@
 double calcFunc(double a, double b, char op) {
 	double result = 0;
 
-	if (op == '+') {
-		result = a + b;
-	}
-	else if (op == '-') {
-		result = a - b;
-	}
-	else if (op == '/') {
-		result = a / b;
-	}
-	else if (op == '*') {
-		result = a * b;
-	}
-	else if (op == '%') {
-		result = (int)a % (int)b;
-	}
-	else if (op == '^') {
-		result = pow(a, b);
-	}
-	else
-	{
-		std::cout << "Invalid Input" << std::endl;
+	switch (op) {
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		case '/':
+			result = a / b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '%':
+			result = (int)a % (int)b;
+			break;
+		case '^':
+			result = pow(a, b);
+			break;
+		default:
+			std::cout << "Invalid Input" << std::endl;
+			break;
 	}
 
 	return result;
